3a891a5452707bb85ad6f2befc8276068d81c4c7
Cain <119138782+starsalmon@users.noreply.github.com>
2023-05-07 15:52:21 +1000

    initial
    first

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align = "center"> T-QT Pro Hatchidea</h1>
+
+## Description
+
+Coding challenge on the T-QT Pro - When a button is pressed, animate a chicken hatching from an egg then randomly show a thumbs up or thumbs down image.
---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include "Arduino.h"
+#include <TFT_eSPI.h>
+#include "image_yes.c"
+#include "image_no.c"
+
+#define SCREEN_WIDTH         128
+#define SCREEN_WIDTH         128
+
+
+#define PIN_BAT_VOLT         4
+#define PIN_BTN_L            0
+#define PIN_BTN_R            47
+#define PIN_LCD_BL           10
+
+uint button_time = 0;
+
+TFT_eSPI tft =  TFT_eSPI();
+
+void show_yes()
+{
+    tft.pushImage(0, 0, 128, 128, gImage_image_yes);
+    delay(20);
+}
+
+void show_no()
+{
+    tft.pushImage(0, 0, 128, 128, gImage_image_no);
+    delay(20);
+}
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Hello T-QT");
+
+    tft.begin();
+    tft.setRotation(2);
+
+    pinMode(PIN_BTN_L, INPUT_PULLUP);
+    pinMode(PIN_BTN_R, INPUT_PULLUP);
+}
+
+void loop()
+{
+
+    if (millis() - button_time > 150)
+    {
+        if (digitalRead(PIN_BTN_L) == 0)
+        {
+            show_yes();
+            button_time = millis();
+        }
+        else if (digitalRead(PIN_BTN_R) == 0)
+        {
+            show_no();
+            button_time = millis();
+        }
+        else
+        {
+            tft.fillScreen(TFT_BLACK);
+            button_time = millis();
+        }
+    }
+
+}
