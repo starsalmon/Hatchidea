7bc7795345dadaba11b3d06a6e991b3a9685bffa
Cain <119138782+starsalmon@users.noreply.github.com>
2023-05-13 22:24:33 +1000

    fading functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include "image_yes.c"
 #include "image_no.c"
 
-#define SCREEN_WIDTH         128
-#define SCREEN_WIDTH         128
+// define this macro to enable debug messages
+#define DEBUG_ENABLED true
 
+#define SCREEN_WIDTH         128
+#define SCREEN_WIDTH         128
 
 #define PIN_BAT_VOLT         4
 #define PIN_BTN_L            0
@@ -13,49 +15,43 @@
 #define PIN_LCD_BL           10
 
 uint button_time = 0;
-int brightness = 0;
+int brightness = 255; // inverted, 255 is off
 
 TFT_eSPI tft =  TFT_eSPI();
 
-void show_yes()
-{
+void fade_bl_on(){
+    for (brightness = 255; brightness >= 0; brightness--) { // decrease brightness
+        analogWrite(PIN_LCD_BL, brightness);  // set the LED brightness
+        delay(1);  // wait a little bit to see the effect
+    }    
+}
+
+void fade_bl_off(){
+    for (brightness = 0; brightness <= 255; brightness++) { // increase brightness
+        analogWrite(PIN_LCD_BL, brightness);  // set the LED brightness
+        delay(4);  // wait a little bit to see the effect
+    }
+}
+
+void show_yes() {
     tft.pushImage(0, 0, 128, 128, image_yes);
-
-    for (brightness = 255; brightness >= 0; brightness--) { // decrease brightness
-        analogWrite(PIN_LCD_BL, brightness);  // set the LED brightness
-        delay(1);  // wait a little bit to see the effect
-    }
-
-    for (brightness = 0; brightness <= 255; brightness++) { // increase brightness
-        analogWrite(PIN_LCD_BL, brightness);  // set the LED brightness
-        delay(5);  // wait a little bit to see the effect
-    }
-
+    fade_bl_on();
+    fade_bl_off();
 }
 
-void show_no()
-{
+void show_no(){
     tft.pushImage(0, 0, 128, 128, image_no);
-
-    for (brightness = 255; brightness >= 0; brightness--) { // decrease brightness
-        analogWrite(PIN_LCD_BL, brightness);  // set the LED brightness
-        delay(1);  // wait a little bit to see the effect
-    }
-
-    for (brightness = 0; brightness <= 255; brightness++) { // increase brightness
-        analogWrite(PIN_LCD_BL, brightness);  // set the LED brightness
-        delay(5);  // wait a little bit to see the effect
-    }
-
+    fade_bl_on();
+    fade_bl_off();
 }
 
 
-
-void setup()
-{
-    Serial.begin(115200);
-    Serial.println("Hello T-QT");
-
+void setup() {    
+    #ifdef DEBUG_ENABLED
+        Serial.begin(115200);
+        Serial.println("Hello T-QT");
+    #endif
+    
     tft.begin();
     tft.setRotation(2);
     tft.setSwapBytes(true); // Required when using pushImage
@@ -65,16 +61,27 @@ void setup()
     pinMode(PIN_LCD_BL, OUTPUT);
 }
 
-void loop()
-{
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//                                          L O O P                                             //
+//////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
     if (millis() - button_time > 150)
     {
         if (digitalRead(PIN_BTN_L) == 0)
         {
+            #ifdef DEBUG_ENABLED
+                Serial.println("Random...");
+            #endif
+
             if (random(2) == 0) {
+                #ifdef DEBUG_ENABLED
+                    Serial.println("Yes!");
+                 #endif
                 show_yes();
             } else {
+                #ifdef DEBUG_ENABLED
+                    Serial.println("No!");
+                 #endif
                 show_no();
             }
             button_time = millis();
@@ -87,7 +94,7 @@ void loop()
         }
         else
         {
-            analogWrite(PIN_LCD_BL, 255);
+            analogWrite(PIN_LCD_BL, brightness);
             tft.fillScreen(TFT_BLACK);
             button_time = millis();
             
